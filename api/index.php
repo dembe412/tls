ac91c9c12a6133bed55c75696cf6f30e2c667d0d
dembe412 <2024bse055@std.must.ac.ug>
2026-09-16 17:55:05 +0300

    support laravel runtime on vercel filesystem

--- api/index.php
+++ api/index.php
@@ -1,9 +1,29 @@
 <?php
 
-try {
-    require __DIR__.'/../public/index.php';
-} catch (Throwable $exception) {
-    error_log($exception::class.': '.$exception->getMessage());
+$runtimeStorage = '/tmp/laravel-storage';
+$runtimeCache = '/tmp/laravel-cache';
 
-    throw $exception;
+foreach ([$runtimeStorage, $runtimeCache] as $directory) {
+    if (! is_dir($directory)) {
+        mkdir($directory, 0755, true);
+    }
 }
+
+$runtimeEnvironment = [
+    'LARAVEL_STORAGE_PATH' => $runtimeStorage,
+    'APP_SERVICES_CACHE' => $runtimeCache.'/services.php',
+    'APP_PACKAGES_CACHE' => $runtimeCache.'/packages.php',
+    'APP_CONFIG_CACHE' => $runtimeCache.'/config.php',
+    'APP_ROUTES_CACHE' => $runtimeCache.'/routes.php',
+    'APP_EVENTS_CACHE' => $runtimeCache.'/events.php',
+    'APP_COMPILED_VIEW_PATH' => $runtimeStorage.'/framework/views',
+    'LOG_CHANNEL' => 'stderr',
+];
+
+foreach ($runtimeEnvironment as $key => $value) {
+    putenv($key.'='.$value);
+    $_ENV[$key] = $value;
+    $_SERVER[$key] = $value;
+}
+
+require __DIR__.'/../public/index.php';
