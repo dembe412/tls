9f241fee61c9f7871680a8fc0d4638168e8e5a80
dembe412 <2024bse055@std.must.ac.ug>
2026-09-16 17:18:11 +0300

    log vercel laravel startup errors

--- api/index.php
+++ api/index.php
@@ -1,3 +1,9 @@
 <?php
 
-require __DIR__.'/../public/index.php';
+try {
+    require __DIR__.'/../public/index.php';
+} catch (Throwable $exception) {
+    error_log($exception::class.': '.$exception->getMessage());
+
+    throw $exception;
+}
